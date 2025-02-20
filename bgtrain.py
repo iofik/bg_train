6ed4361fc8caf2ccc2b72d2d68523c73e9871235
Vladimir Iofik <v.iofik@gmail.com>
2025-02-20 21:02:38 +0100

    Compare exceptional strength in addition to total roll

--- bgtrain.py
+++ bgtrain.py
@@ -2,9 +2,14 @@
 REROLL_TEXT = 'REROLL'
 TESSERACT_HDR = ['level', 'page_num', 'block_num', 'par_num', 'line_num',
         'word_num', 'left', 'top', 'width', 'height', 'conf', 'text']
-TOTAL_ROLL_OFF = 122, -72
-TOTAL_ROLL_SIZE = 40, 26
-DEFAULT_THRESHOLD = 90
+TOTAL_REG_OFF = 122, -72
+TOTAL_REG_SIZE = 40, 26
+STR_REG_OFF = -88, 0
+STR_REG_SIZE = 60, 28
+CHA_DEC_OFF = 268, -120
+ABIL_BUT_OFF = -48, -54
+TOTAL_THRESHOLD = 90
+EXCSTR_THRESHOLD = 76
 
 from PIL import ImageGrab
 import sys
@@ -13,15 +18,15 @@ import numpy as np
 import pytesseract
 import pyautogui
 
-def prepare_image(screenshot):
-    image = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+def prepare_image(image):
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
     return thresh
 
 def find_reroll():
-    screenshot = ImageGrab.grab()
-    img = prepare_image(screenshot)
+    img = ImageGrab.grab()
+    img = prepare_image(img)
     img_data = pytesseract.image_to_data(img)
     img_data = [t.split('\t') for t in img_data.split('\n')]
     assert img_data[-1] == ['']
@@ -35,35 +40,88 @@ def find_reroll():
             for k in ('left', 'top', 'width', 'height')]
     return left + wdth//2, top + hght//2
 
-def get_number_from_image(screenshot):
-    img = prepare_image(screenshot)
+def get_total(region):
+    img = ImageGrab.grab(bbox=region)
+    img = prepare_image(img)
     custom_config = r'--oem 3 --psm 6 outputbase digits'
-    number_str = pytesseract.image_to_string(img, config=custom_config).strip()
-    try:
-        return int(number_str)
-    except ValueError:
-        return None
+    total_str = pytesseract.image_to_string(img, config=custom_config).strip()
+    return int(total_str)
 
-def main(threshold):
+def get_excstr(region):
+    img = ImageGrab.grab(bbox=region)
+    img = prepare_image(img)
+    custom_config = r'--oem 3 --psm 6 outputbase'
+    strexc_str = pytesseract.image_to_string(img, config=custom_config).strip()
+    assert len(strexc_str) == 5 and strexc_str[:3] == '18/'
+    excstr = int(strexc_str[3:])
+    return excstr or 100
+
+def calc_total_region(button):
+    region = [button[i] + TOTAL_REG_OFF[i] for i in (0, 1)]
+    region.extend([region[i] + TOTAL_REG_SIZE[i] for i in (0, 1)])
+    return region
+
+def calc_dec_buttons(reroll_button):
+    cha_dec = [reroll_button[i] + CHA_DEC_OFF[i] for i in (0, 1)]
+    abil_dec = [(cha_dec[0], cha_dec[1] + i*ABIL_BUT_OFF[1]) for i in range(5)]
+    return abil_dec
+
+def calc_inc_button(dex_dec_button):
+    str_inc = tuple(dex_dec_button[i] + ABIL_BUT_OFF[i] for i in (0, 1))
+    return str_inc
+
+def calc_str_region(str_inc_button):
+    x, y = [str_inc_button[i] + STR_REG_OFF[i] for i in (0, 1)]
+    return [x-STR_REG_SIZE[0]//2, y-STR_REG_SIZE[1]//2,
+            x+STR_REG_SIZE[0]//2, y+STR_REG_SIZE[1]//2]
+
+def show_excstr(dec_buttons, inc_button):
+    DEC_CLICKS = 5
+    INC_CLICKS = 15
+    pos = pyautogui.position()
+    for button in dec_buttons:
+        pyautogui.moveTo(button)
+        for i in range(DEC_CLICKS):
+            if pyautogui.position() != button:
+                return False
+            pyautogui.click()
+    pyautogui.moveTo(inc_button)
+    for i in range(INC_CLICKS):
+        if pyautogui.position() != inc_button:
+            return False
+        pyautogui.click()
+    pyautogui.moveTo(pos)
+    return True
+
+def main(total_threshold, excstr_threshold):
     button = find_reroll()
-    print(f"Threshold: {threshold}")
+    print(f"Threshold: {total_threshold}/{excstr_threshold}")
     print(f"Reroll button at: {button}")
-    region = [button[i] + TOTAL_ROLL_OFF[i] for i in (0, 1)]
-    region.extend([region[i] + TOTAL_ROLL_SIZE[i] for i in (0, 1)])
+    total_region = calc_total_region(button)
+    dec_buttons = calc_dec_buttons(button)
+    inc_button = calc_inc_button(dec_buttons[-1])
+    str_region = calc_str_region(inc_button)
     pyautogui.moveTo(button)
     print(f"Total Roll:", end=' ', flush=True)
     while True:
-        screenshot = ImageGrab.grab(bbox=region)
-        number = get_number_from_image(screenshot)
-        print(number, end=' ', flush=True)
-        if number is not None and number >= threshold:
-            print('- found!')
-            break
+        total = get_total(total_region)
+        if total < total_threshold:
+            print(total, end=' ', flush=True)
+        else:
+            if not show_excstr(dec_buttons, inc_button):
+                print('- break!')
+                break
+            excstr = get_excstr(str_region)
+            print(f"{total}/{excstr}", end=' ', flush=True)
+            if excstr >= excstr_threshold:
+                print('- found!')
+                break
         if pyautogui.position() != button:
             print('- break!')
             break
         pyautogui.click()
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_THRESHOLD)
+    main(int(sys.argv[1]) if len(sys.argv) > 1 else TOTAL_THRESHOLD,
+         int(sys.argv[2]) if len(sys.argv) > 2 else EXCSTR_THRESHOLD)
 
